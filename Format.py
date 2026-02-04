6d89aa8a0e996fa6a4305005eb61c1d765b5f56d
Najmul Akash <najmulbalghari23@gmail.com>
2026-02-04 00:51:56 -0800

    python sam

---
+++ Format.py
@@ -0,0 +1,15 @@
+name="najmul akash"
+age="23"
+print("Hello"+name+"Your age is"+age)
+
+#use full python 3
+
+name="tajwar shaheen"
+age="12"
+print("Hello {} your age is {}".format(name,age))
+
+#python 3.6 most use full
+
+name="akash"
+age="24"
+print(f"Hello {name} your age is {age}")
---
+++ Split.py
@@ -0,0 +1,8 @@
+name = "najmul"
+age = "34"
+print("Hello" +name+"Your age is"+age)
+
+#usefull 
+name=input("Enter your name")
+age =input ("Enter your age")
+print(name,age)
