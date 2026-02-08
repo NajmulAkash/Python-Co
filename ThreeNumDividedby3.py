dc489a389aee0580012fc5f32e0d7003276b6ab4
Najmul Akash <najmulbalghari23@gmail.com>
2026-02-08 04:04:10 -0800

    portfolio

---
+++ ThreeNumDividedby3.py
@@ -0,0 +1,4 @@
+num1=input("Enter your First Number")
+num2=input("Enter your Secound Number")
+num3=input("Enter your Thirds Number") 
+print(f"Sum Number Divided by three is Equal to{(int(num1)+ int(num2)+ int(num3))/3 }")
