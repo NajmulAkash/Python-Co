2b99f15082ae099e32d46bce8efdf0dae8985bcb
Najmul Akash <najmulbalghari23@gmail.com>
2026-02-10 07:53:18 -0800

    py

---
+++ Solution.py
@@ -0,0 +1,2 @@
+name = input("enter your name :")
+print(f"slicing name is {name [-1::-1]}")n
---
+++ String_slicing.py
@@ -0,0 +1,7 @@
+language = "Python"
+print(language[0])
+
+lang ="graphic"
+print(lang[0::4])
+
+print("Math"[2])
