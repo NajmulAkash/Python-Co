c24c511e8c17045c05e6719c1d934b9fe4a80651
Najmul Akash <najmulbalghari23@gmail.com>
2026-02-13 08:14:34 -0800

    lenght

--- Length.py
+++ Length.py
@@ -1,16 +1,17 @@
-#length
-name="NaJmUl aKAsH"
-len=(name NaJmUl aKAsH)
-print
 
-#Lower Kase Small Later ma laya ga
+name="NaJmUl aKAsH"
+length=len(name)
+print(name)
+
+
+print(len(name))
+
 print(name.lower())
 
-#Upper Kasa Captail later ma laya ga
 print(name.upper())
 
-#Title kasa First later capital ma or bake sab small ma laya ga
+
 print(name.title())
 
-#count kasa is ma string ma same alphabat ko count kara ga
+
 print(name.count("a"))
